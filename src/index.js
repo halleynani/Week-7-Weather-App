2bb0e4c0a980ba0751fb6aec22243e20c122887d
halleynani <nanihalley@gmail.com>
2025-07-10 18:44:34 -0500

    Fixed search input JS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 function handleSearchSubmit(event) {
     event.preventDefault();
-    let searchForm = document.querySelector("#search-form-input");
+    let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
-}
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+  }
+  
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
