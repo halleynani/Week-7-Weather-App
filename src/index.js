1f9c96e493448fded6dba4068a14bdcd38f96387
halleynani <nanihalley@gmail.com>
2025-07-16 19:06:07 -0500

    Inject forecast html

--- src/index.js
+++ src/index.js
@@ -52,25 +52,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast () {
-  let forecast = document.querySelector("#forecast");
+function displayForecast(response) {
 
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    days.forEach(function (day) {
-      forecast.innerHTML = `
-           <div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">⛅️</div>
-            <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-temperature">
-              <strong>15°</strong>
-              </div>
-              <div class="weather-forecast-temperature">9°</div>
-            </div>
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
           </div>
-        `;
-    });
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#search-form");
